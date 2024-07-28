89ccc3fb8038b72f4102bd32955bd044683986e9
Karan Tushar <tusharkaran99@gmail.com>
2024-07-28 01:30:36 -0400

    new changes for the ADT

--- src/components/TicketAnalysis.js
+++ src/components/TicketAnalysis.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Icon from './Icon';
 
 const TicketAnalysis = () => {
-    const [countryData, setCountryData] = useState([]);
+    const [algoData, setAlgoData] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:8000/api/analytics/GetAlgoDetails')
@@ -12,13 +12,13 @@ const TicketAnalysis = () => {
                 const formattedData = Object.values(data).map((algo, index) => ({
                     id: index + 1,
                     name: algo.name,
-                    rise: algo.avgCompressionTime < 15000, // example condition, adjust as needed
+                    isEfficient: algo.avgCompressionTime < 15000, // example condition, adjust as needed
                     compressionTime: algo.avgCompressionTime,
                     decompressionTime: algo.avgDecompressionTime,
                 }));
-                setCountryData(formattedData);
+                setAlgoData(formattedData);
             })
-            .catch(error => console.error('Error fetching country data:', error));
+            .catch(error => console.error('Error fetching algorithm data:', error));
     }, []);
 
     return (
@@ -28,21 +28,17 @@ const TicketAnalysis = () => {
                 <Icon path="res-react-dash-plus" className="w-5 h-5" />
             </div>
             <div className="flex font-bold mb-2">
-                {/* <div className="w-12">ID</div> */}
                 <div className="flex-grow ml-2">Compression Algorithm</div>
                 <div className="w-40">Compression Time</div>
                 <div className="w-40">Decompression Time</div>
                 <div className="w-12"></div>
                 <div className="w-6"></div>
             </div>
-            {countryData.map(({ id, name, rise, compressionTime, decompressionTime }) => (
+            {algoData.map(({ id, name, isEfficient, compressionTime, decompressionTime }) => (
                 <div className="flex items-center mt-3" key={id}>
-                    {/* <div className="w-12">{id}</div> */}
                     <div className="flex-grow ml-2">{name}</div>
                     <div className="w-40">{`${compressionTime.toLocaleString()} ms`}</div>
                     <div className="w-40 ml-4">{`${decompressionTime.toLocaleString()} ms`}</div>
-                    {/* <Icon path={rise ? "res-react-dash-country-up" : "res-react-dash-country-down"} className="w-4 h-4 mx-3" />
-                    <Icon path="res-react-dash-options" className="w-2 h-2" /> */}
                 </div>
             ))}
             <div className="flex-grow" />
